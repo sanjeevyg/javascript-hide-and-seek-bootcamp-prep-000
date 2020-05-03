2c331f5cdbca83979341e05d91b362e7fddadae4
smiling-animation-2421 <yogi.sjv@gmail.com>
2020-05-03 10:21:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector) {
+  let firstElement = document.querySelector(selector)
+  return firstElement
+}
 
+function nestedTarget() {
+  let nestedElement = document.getElementById("nested").querySelector(".target")
+  return nestedElement
+}
+
+function increaseRankBy(n) {
+  let lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString()
+  }
+}
